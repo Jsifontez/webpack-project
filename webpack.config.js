5e978334b0ac23a802194b40664e48e0ad61e675
Juan Sifontez <juan.sifontez.dev@gmail.com>
2022-10-05 15:27:28 -0400

    Add initial webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path')
+
+module.exports = {
+  // entry: path(__dirname, )
+  entry: './src/index.js',
+  output: {
+    // resolve is used to know where our project is based
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  resolve: {
+    extensions: ['.js']
+  }
+}
